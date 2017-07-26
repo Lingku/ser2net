0c6a883faac0db93fa1b55e6c5dfd7d45dbff074
Xia Haiqiang <xiahaiqiang@yuanwang.com>
2017-07-26 09:38:10 +0800

    bug fix: if device name contains colon, load configure failed

--- readconfig.c
+++ readconfig.c
@@ -978,7 +978,14 @@ handle_config_line(char *inbuf, int len)
 	goto out;
     }
 
-    devname = strtok_r(NULL, ":", &strtok_data);
+	/*
+		device name string may contains char ':'
+		eg. '/dev/serial/by-path/pci-0000:00:1d.1-usb-0:2.1:1.0-port0'
+	*/
+    if (strtok_data[0] == '\'')
+        devname = strtok_r(NULL, "'", &strtok_data);
+    else
+        devname = strtok_r(NULL, ":", &strtok_data);
     if (devname == NULL) {
 	syslog(LOG_ERR, "No device name given on line %d", lineno);
 	goto out;
